0c4550969439f89a7a83302d2766f93aed20f3dc
Kaya <sergazievadil@gmail.com>
2023-05-11 21:57:38 +0600

    Initial commit

---
+++ app/src/main/java/com/example/fragment/FourthFragment.kt
@@ -0,0 +1,29 @@
+package com.example.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+
+class FourthFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_fourth, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/fragment/ThirdFragment.kt
@@ -0,0 +1,33 @@
+package com.example.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+
+class ThirdFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_third, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.third_Button).setOnClickListener {
+
+            findNavController().navigate(R.id.action_thirdFragment_to_fourthFragment)
+
+
+        }
+
+
+    }
+}
